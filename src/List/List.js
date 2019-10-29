a932f771f1370345b30934ff174dd11f3acba545
BraydenGray416 <brayden.gray98@gmail.com>
2019-10-29 14:43:41 +1300

    changed to let it read from items list

--- src/List/List.js
+++ src/List/List.js
@@ -4,15 +4,47 @@ import Form from '../Form/Form';
 import Item from '../Item/Item';
 
 class List extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            allItems: [
+                {
+                    id: 1,
+                    text: 'Apples'
+                },
+                {
+                    id: 2,
+                    text: 'Bananas'
+                },
+                {
+                    id: 3,
+                    text: 'Pears'
+                },
+                {
+                    id: 4,
+                    text: 'Beers'
+                }
+            ]
+        }
+    }
 
     render(){
+        console.log(this.state.allItems);
         return(
             <div className="listContainer">
-                <h1>Shopping List</h1>
-                <Form />
-                <div className="list">
-                    <Item />
-                </div>
+            <h1>Shopping List</h1>
+            <Form />
+            <div className="list">
+            {
+                this.state.allItems.map(singleItem => {
+                    return <Item
+                    key={singleItem.id}
+                    itemInfo={singleItem}
+                    />
+                })
+            }
+            </div>
             </div>
         )
     }
